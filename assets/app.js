34c0aaa5b4b23dc8f33d222024c60de0f2480f8e
Veneruso <Diego.Veneruso@sbw-media.ch>
2024-10-24 09:23:46 +0200

    Commit

---
+++ assets/app.js
@@ -0,0 +1,35 @@
+const register = Vue.createApp({
+    data() {
+        return {
+            studentName: 'JOki',
+            vacation: {
+                VacationTypeID: 1,
+                StudentID: 277,
+                FromDate: "0000-00-00",
+                ToDate: "000-00-00",
+            }    
+        }
+    },
+    methods: {
+        async addVacation() {
+            const url='https://api-sbw-plc.sbw.media/Vacation';
+            const response = await fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(this.vacation),
+            });
+            if (response.ok) {
+                console.log('Vacation added successfully');
+            } else {
+                console.error('Failed to add vacation');
+            }
+            
+        }
+    },
+    mounted() {
+        this.vacation.StudentID = localStorage.getItem('StudentID');
+    }
+
+}).mount('#register');
